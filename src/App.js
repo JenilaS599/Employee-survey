ebffc1333da90203ae2a4e3e4817130280f00cdd
JenilaS599 <84403727+JenilaS599@users.noreply.github.com>
2021-07-19 08:06:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Form from './form';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import employees from './pages/employees';
 
 function App() {
   return (
     <div>
-      <Form/>
+      <Router>
+        <Switch>
+          <Route path="/" exact component = {Form}/>
+          <Route path="/employees" exact component={employees}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
