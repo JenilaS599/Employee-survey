c43681fbb1f24f43f60b526e42a3507e24b664aa
JenilaS599 <84403727+JenilaS599@users.noreply.github.com>
2021-07-26 12:08:48 +0530

    Update employees.js

--- src/pages/employees.js
+++ src/pages/employees.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios";
 
-export default function employees() {
+export default function Employees() {
+    const [EmployeeData, setEmployeeData] = useState('');
+
+    const getEmployeeData = () =>{
+        axios.get('http://localhost:5000/employees/')
+        .then((res) => {
+            console.log(res.data);
+            setEmployeeData(res.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+    
+    useEffect(() =>{
+        getEmployeeData();
+    }, []);
+
     return (
         <div>
-            <h1>List of registered employees</h1>
+            {EmployeeData? (EmployeeData.map((EmployeeData) =>{
+                return(
+                    <div>
+                        <h3>{EmployeeData.name}
+                        {EmployeeData.empid}
+                        {EmployeeData.role}
+                        {EmployeeData.company}
+                        </h3>
+                        </div>
+                );
+            })):(
+                <span></span>
+            )}
         </div>
     )
 }
