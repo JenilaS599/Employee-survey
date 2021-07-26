9a128267e26834a32820a5f6164b4fc729f63195
JenilaS599 <84403727+JenilaS599@users.noreply.github.com>
2021-07-26 20:42:07 +0530

    Update employees.js

--- src/pages/employees.js
+++ src/pages/employees.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 
 export default function Employees() {
@@ -23,22 +24,23 @@ export default function Employees() {
 
     return (
         <div>
+            <h1 class="table-head">List of Employees</h1>
+            <table>
+        <tr>
+            <th className="col-md-3">Employee Name</th>
+            <th className="col-md-3">Employee Id</th>
+            <th className="col-md-3">Role</th>
+            <th className="col-md-3">Select</th>
+        </tr></table>
             {EmployeeData? (EmployeeData.map((EmployeeData) =>{
                 return(
           <div>
-              <h1 class="table-head">List of Employees</h1>
-      <table>
+     <table>
         <tr>
-            <th>Employee Name</th>
-            <th>Employee Id</th>
-            <th>Role</th>
-            <th>Company Name</th>
-        </tr>
-        <tr>
-            <td>{EmployeeData.name}</td>
-            <td>{EmployeeData.empid}</td>
-            <td>{EmployeeData.role}</td>
-            <td>{EmployeeData.company}</td>
+            <td className="col-md-3">{EmployeeData.name}</td>
+            <td className="col-md-3">{EmployeeData.empid}</td>
+            <td className="col-md-3">{EmployeeData.role}</td>
+            <input className="col-md-3" type="checkbox"></input>
         </tr>
     </table>
    </div>
