ec81d1c0a315b49656347ed5692bf5322680de34
JenilaS599 <84403727+JenilaS599@users.noreply.github.com>
2021-07-26 20:42:44 +0530

    Create HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Navbar from './navbar'
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar/>
+            </div>
+        )
+    }
+}
