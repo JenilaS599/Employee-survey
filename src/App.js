8cedcb0d1e99f9f888c0aca511672374fe3b839f
JenilaS599 <84403727+JenilaS599@users.noreply.github.com>
2021-07-27 10:18:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import employees from './pages/employees';
 import Navbar from './pages/navbar';
 import Signup from './pages/signup';
 import Signin from './pages/signin';
+import Employees from './pages/employees';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div>
         <Router>
-        <Navbar/>
         <Switch>
+        <Route path="/" exact component = {HomePage}/>
           <Route path="/signup" exact component = {Signup}/>
           <Route path="/signin" exact component = {Signin}/>
-          <Route path="/employees" exact component={employees}/>
+          <Route path="/employees" exact component={Employees}/>
         </Switch>
       </Router>
     </div>
